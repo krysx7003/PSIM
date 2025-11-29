13476b9e227aa483771d40aadaab0a79a28fe5ea
Michuupk <michal.pakula3@gmail.com>
2025-11-29 20:25:05 +0100

    Working BLE finding (todo fix)

--- composeApp/src/androidMain/kotlin/com/napnap/heartbridge/ui/components/DialogBox.kt
+++ composeApp/src/androidMain/kotlin/com/napnap/heartbridge/ui/components/DialogBox.kt
@@ -39,13 +39,6 @@ fun DialogBox(devices: List<BluetoothDevice>, onDismissRequest: () -> Unit ){
             Manifest.permission.BLUETOOTH_CONNECT
         ) != PackageManager.PERMISSION_GRANTED
     ) {
-        // TODO: Consider calling
-        //    ActivityCompat#requestPermissions
-        // here to request the missing permissions, and then overriding
-        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-        //                                          int[] grantResults)
-        // to handle the case where the user grants the permission. See the documentation
-        // for ActivityCompat#requestPermissions for more details.
         return
     }
     BasicAlertDialog(
